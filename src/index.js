f67152afa5eca13c84ddcfaea16f0317fb2a488e
Razvanelm <razvanelm7@gmail.com>
2024-01-16 17:49:52 +0200

    changing

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './Containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
@@ -9,7 +10,9 @@ import 'tachyons';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App/>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
